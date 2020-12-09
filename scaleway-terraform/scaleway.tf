18f95175c0f1d4426dc69818cb2e7f6bc7fdeff0
Khaled Abdulhalim <lga.sig.khaled@gmail.com>
2020-12-09 23:07:16 +0100

    terfform-scalway

---
+++ scaleway-terraform/scaleway.tf
@@ -0,0 +1,26 @@
+provider "scaleway" {
+  access_key      = "SCW7Q77VGY2NB0BRVBGG"
+  secret_key      = "e4f60e72-b97d-403a-8a24-1921ee06a17e"
+  organization_id = "4db4430e-bffa-48fa-a5f7-6554bd128cf8"
+  zone            = "fr-par-1"
+  region          = "fr-par"
+}
+
+resource "scaleway_instance_ip" "public_ip" {}
+
+resource "scaleway_instance_volume" "data" {
+  size_in_gb = 550
+  type = "l_ssd"
+}
+
+resource "scaleway_instance_server" "my-ubuntu-instance" {
+  type  = "GP1-M"
+  image = "ubuntu-focal"
+
+  tags = [ "FocalFossa", "MyUbuntuInstance" ]
+
+  ip_id = scaleway_instance_ip.public_ip.id
+
+  additional_volume_ids = [ scaleway_instance_volume.data.id ]
+
+}
