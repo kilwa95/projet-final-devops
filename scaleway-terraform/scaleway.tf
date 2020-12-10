f7b0420dee4a342772dca507c4a79a744d829864
Khaled Abdulhalim <lga.sig.khaled@gmail.com>
2020-12-10 09:13:58 +0100

    Update_scalway

--- scaleway-terraform/scaleway.tf
+++ scaleway-terraform/scaleway.tf
@@ -1,3 +1,5 @@
+
+
 provider "scaleway" {
   access_key      = "SCW7Q77VGY2NB0BRVBGG"
   secret_key      = "e4f60e72-b97d-403a-8a24-1921ee06a17e"
@@ -13,6 +15,26 @@ resource "scaleway_instance_volume" "data" {
   type = "l_ssd"
 }
 
+resource "scaleway_instance_security_group" "my-security-group" {
+  inbound_default_policy  = "drop"
+  outbound_default_policy = "accept"
+
+  inbound_rule {
+    action = "accept"
+    port   = "22"
+  }
+
+  inbound_rule {
+    action = "accept"
+    port   = "80"
+  }
+
+  inbound_rule {
+    action = "accept"
+    port   = "443"
+  }
+}
+
 resource "scaleway_instance_server" "my-ubuntu-instance" {
   type  = "GP1-M"
   image = "ubuntu-focal"
@@ -22,5 +44,7 @@ resource "scaleway_instance_server" "my-ubuntu-instance" {
   ip_id = scaleway_instance_ip.public_ip.id
 
   additional_volume_ids = [ scaleway_instance_volume.data.id ]
+  security_group_id = scaleway_instance_security_group.my-security-group.id
 
 }
+
